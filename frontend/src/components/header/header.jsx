7b9f5068ae51bf7c0b50b90160b7b2e845ef4f98
siddhantprateek <siddhantprateek@gmail.com>
2024-08-18 09:22:09 +0530

    header removed

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='header'>
-      <header className="bg-white p-4 shadow-md sticky top-0 z-10">
-        <h1 className="text-2xl font-bold text-blue-600">Company.ai</h1>
+      <header className="bg-white shadow-md sticky top-0 z-10">
+        {/* <h1 className="text-2xl font-bold text-blue-600">Company.ai</h1> */}
       </header>
-    </div>
   )
 }
 
