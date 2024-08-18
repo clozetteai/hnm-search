2ac3164bfff23066e1dc975d7dd4965e4a06eb42
siddhantprateek <siddhantprateek@gmail.com>
2024-08-18 15:33:14 +0530

    setting page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Chat } from './pages';
-// import { Routes, Route } from 'react-router-dom';
+import { Chat, Setting } from './pages';
+// import { Route } from 'lucide-react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App w-full h-screen">
-      <Chat />
+    <div className="App w-full h-screen bg-gray-100">
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/settings" element={<Setting />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './contexts/auth';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
